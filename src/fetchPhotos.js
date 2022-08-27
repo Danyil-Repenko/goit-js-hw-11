c7976edaa6445a1eabf00e8b1da1b438e5fca467
Danyil-Repenko <danyil.oficial@gmail.com>
2022-08-27 17:34:24 +0300

    крок 2

--- src/fetchPhotos.js
+++ src/fetchPhotos.js
@@ -1,13 +1,15 @@
-export function fetchPhotos(name) {
+export function fetchPhotos(name, page) {
     const rootUrl = "https://pixabay.com/api/";
-    // const peremeters = {
-    //     key: '29417060 - 6945200ead3992d525ee3c3b8',
-    //     q: name,
-    //     image_type: 'photo',
-    //     orientation: 'horizontal',
-    //     safesearch: true,
-    // };
-    return fetch(`${rootUrl}`)
+    const peremeters = new URLSearchParams({
+        key: '29417060-6945200ead3992d525ee3c3b8',
+        q: name,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page
+    });
+    return fetch(`${rootUrl}?${peremeters}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -3,20 +3,47 @@ import { fetchPhotos } from './fetchPhotos.js';
 import { Notify } from 'notiflix';
 
 const refs = {
-    form: document.querySelector("#search-form"),
-    queryInput: document.querySelector("[name='searchQuery']"),
-    gallery: document.querySelector(".gallery"),
+  form: document.querySelector("#search-form"),
+  queryInput: document.querySelector("[name='searchQuery']"),
+  gallery: document.querySelector("div.gallery"),
+  loadMoreButton: document.querySelector("button.load-more"),
 }
-
 
 refs.form.addEventListener('submit', onSearch);
 
 function onSearch(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const inputValue = event.currentTarget.elements.searchQuery.value;
+  const inputValue = event.currentTarget.elements.searchQuery.value.trim();
 
-    fetchPhotos(inputValue)
-        .then(data => console.log)
-        .catch(error => console.log)
+  fetchPhotos(inputValue, page)
+    .then(data => {
+      if (data.hits.length === 0) {
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        return
+      }
+      const markup = data.hits
+        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+          return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+  </div>
+</div>`
+        })
+        .join('')
+      refs.gallery.innerHTML = markup;
+    })
+    .catch(() => Notify.warning("Sorry, an error occurred on the server. Please try again some time later."))
 }
